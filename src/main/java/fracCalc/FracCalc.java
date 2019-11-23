6ca0e542304c5c94d1c6446d601c81fc144da119
s-uchidam <s-uchidam@53S50031122.it.bsd405.org>
2019-11-22 21:28:38 -0800

    Turning in checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -5,10 +5,15 @@ import java.util.*;
 public class FracCalc {
 
 	public static void main(String[] args) {
-		Scanner UserInput = new Scanner(System.in);
-		String input = UserInput.nextLine();
-		System.out.println(produceAnswer(input));
-		UserInput.close();
+		Scanner userInput = new Scanner(System.in);
+		String input = "";
+		input = userInput.nextLine();
+		while (!input.equals("quit")) {
+			System.out.print(produceAnswer(input));
+			System.out.println();
+			input = userInput.nextLine();
+		}
+		userInput.close();
 	}
 
 	public static String produceAnswer(String input) {
@@ -21,6 +26,38 @@ public class FracCalc {
 		String input1 = input.substring(0, i);
 		String operation = input.substring(i + 1, i + 2);
 		String input2 = input.substring(i + 3);
-		return input2;
+		
+		String lists = "whole:" + whole(input2) + " numerator:" + numerator(input2) + " denominator:" + denominator(input2);
+		return lists;
+	}
+	
+	public static int whole(String fraction) {
+		int whole = 0;
+		if (fraction.indexOf("/") == -1) {
+			whole = Integer.parseInt(fraction);
+		} else {
+			if (fraction.indexOf("_") != -1) {
+				whole = Integer.parseInt(fraction.substring(0, fraction.indexOf("_")));
+			}
+		}
+		return whole;
+	}
+	public static int numerator(String fraction) {
+		int numerator = 0;
+		if (fraction.indexOf("/") != -1) {
+			if (fraction.indexOf("_") != -1) {
+				numerator = Integer.parseInt(fraction.substring(fraction.indexOf("_") + 1, fraction.indexOf("/")));
+			} else {
+				numerator = Integer.parseInt(fraction.substring(0, fraction.indexOf("/")));
+			}
+		}
+		return numerator;
+	}
+	public static int denominator(String fraction) {
+		int denominator = 1;
+		if (fraction.indexOf("/") != -1) {
+			denominator = Integer.parseInt(fraction.substring(fraction.indexOf("/") + 1));
+		}
+		return denominator;
 	}
 }
