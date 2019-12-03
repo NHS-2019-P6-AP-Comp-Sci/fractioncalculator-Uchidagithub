2c65bc264301e241ace5eb755b1af989337dfc95
s-uchidam <s-uchidam@53S50031122.it.bsd405.org>
2019-12-03 15:23:03 -0800

    Checkpoint 3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -26,11 +26,50 @@ public class FracCalc {
 		String input1 = input.substring(0, i);
 		String operation = input.substring(i + 1, i + 2);
 		String input2 = input.substring(i + 3);
-		
-		String lists = "whole:" + whole(input2) + " numerator:" + numerator(input2) + " denominator:" + denominator(input2);
-		return lists;
+		int numerator1 = 0;
+		int denominator1 = 0;
+		int numerator2 = 0;
+		int denominator2 = 0;
+		if (whole(input1) >= 0) {
+			numerator1 = numerator(input1) + (whole(input1) * denominator(input1));
+			denominator1 = denominator(input1);
+		}
+		if (whole(input1) < 0) {
+			numerator1 = -1 * (numerator(input1) - (whole(input1) * denominator(input1)));
+			denominator1 = denominator(input1);
+		}
+		if (whole(input2) >= 0) {
+			numerator2 = numerator(input2) + (whole(input2) * denominator(input2));
+			denominator2 = denominator(input2);
+		}
+		if (whole(input2) < 0) {
+			numerator2 = -1 * (numerator(input2) - (whole(input2) * denominator(input2)));
+			denominator2 = denominator(input2);
+		}
+		String answer = "";
+		if (operation.equals("+")) {
+			answer = addition(numerator1, denominator1, numerator2, denominator2);
+		} else if (operation.equals("-")) {
+			answer = subtraction(numerator1, denominator1, numerator2, denominator2);
+		} else if (operation.equals("*")) {
+			answer = multipulcation(numerator1, denominator1, numerator2, denominator2);
+		} else if (operation.equals("/")) {
+			answer = devision(numerator1, denominator1, numerator2, denominator2);
+		}
+		/* int ansNum = numerator(answer);
+		int ansDen = denominator(answer);
+		int ansWhole = ansNum / ansDen;
+		int finalNum = ansNum % ansDen;
+		while ()
+		for (i = ansDen; i > 1; i--) {
+			if (finalNum % i == 0 && ansDen % i == 0) {
+				finalNum /= i;
+				ansDen /= i;
+			}
+		}*/
+		return answer;
 	}
-	
+
 	public static int whole(String fraction) {
 		int whole = 0;
 		if (fraction.indexOf("/") == -1) {
@@ -42,6 +81,7 @@ public class FracCalc {
 		}
 		return whole;
 	}
+
 	public static int numerator(String fraction) {
 		int numerator = 0;
 		if (fraction.indexOf("/") != -1) {
@@ -53,11 +93,44 @@ public class FracCalc {
 		}
 		return numerator;
 	}
+
 	public static int denominator(String fraction) {
 		int denominator = 1;
 		if (fraction.indexOf("/") != -1) {
 			denominator = Integer.parseInt(fraction.substring(fraction.indexOf("/") + 1));
 		}
 		return denominator;
+	}
+
+	public static String addition(int num1, int den1, int num2, int den2) {
+		int newDen = den1 * den2;
+		int newNum1 = num1 * den2;
+		int newNum2 = num2 * den1;
+		int ansNum = newNum1 + newNum2;
+		String answer = ansNum + "/" + newDen;
+		return answer;
+	}
+
+	public static String subtraction(int num1, int den1, int num2, int den2) {
+		int newDen = den1 * den2;
+		int newNum1 = num1 * den2;
+		int newNum2 = num2 * den1;
+		int ansNum = newNum1 - newNum2;
+		String answer = ansNum + "/" + newDen;
+		return answer;
+	}
+
+	public static String multipulcation(int num1, int den1, int num2, int den2) {
+		int newDen = den1 * den2;
+		int newNum = num1 * num2;
+		String answer = newNum + "/" + newDen;
+		return answer;
+	}
+
+	public static String devision(int num1, int den1, int num2, int den2) {
+		int newDen = den1 * num2;
+		int newNum = num1 * den2;
+		String answer = newNum + "/" + newDen;
+		return answer;
 	}
 }
